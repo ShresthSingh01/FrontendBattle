c49ed7ffb0b1fdd2b417ad60ac1fbd8c5dadda95
SHRESTH SINGH <shresthsingh112233@gmail.com>
2026-06-26 15:46:28 +0530

    feat(02): header, hero and core layout implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
+import PageSpine from "./components/layout/PageSpine";
+import Header from "./components/layout/Header";
+import Hero from "./components/sections/Hero";
+
 function App() {
   return (
-    <>
-      <div className="page-spine">
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <main>
+    <div className="bg-[#0a0a0a] min-h-screen text-white font-sans selection:bg-forsythia selection:text-black">
+      <PageSpine />
+      <Header />
+      <main className="relative z-10">
+        <Hero />
+        {/* Future sections will go here */}
       </main>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
